bb97a42cccd76d6fd20a1836eaf554ae43f6e129
lubos64 <arabellalubo61@gmail.com>
2025-09-22 16:12:10 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+from pyscript import display, document
+
+def av_ee(e):
+   num1 = float(document.getElementById("name").value)
+   num2 = float(document.getElementById("old").value)
+   num3 = float(document.getElementById("sch").value)
+   display(result, target = "name", )
+   
+        display("You're old", target = "age", )
+
+
+             display("School name", target = "school", )
+
+multiline = ''' .. Is your name
+You are ... years old
+You belong to ..
+'''
+
+text1 = "Name"
+text2 = "age"
+display(text1)
